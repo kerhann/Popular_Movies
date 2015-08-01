2877143e935211cb01a556281f3c0b1b23b5e92e
kerhann <ULtc=gb6>
2015-08-01 11:58:40 +0200

    Initial commit - nothing done

---
+++ app/src/main/java/be/julot/popularmovies/PosterGridFragment.java
@@ -0,0 +1,26 @@
+package be.julot.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Julot on 31/07/2015.
+ */
+public class PosterGridFragment extends Fragment {
+
+    public PosterGridFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
