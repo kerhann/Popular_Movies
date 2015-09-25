2f1aa9dc9ebff263acec318749f5c7ed9a64f86b
kerhann <jferet@gmail.com>
2015-09-25 14:52:44 +0200

    Attacking stage 2

---
+++ app/src/main/java/be/julot/popularmovies/DB_Favorite_Movies.java
@@ -0,0 +1,25 @@
+package be.julot.popularmovies;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+/**
+ * Created by julot on 25/09/15.
+ */
+
+
+// I made the choice to only record locally the movie ID on TMDB because:
+// 1) I need anyway to make an API call to get the right rating and number of votes (changes daily
+// and can't reasonably be locally stored)
+// 2) I don't want to locally store the movie poster image, hence I need to make an API call anyway
+// for this too
+    //So, my favorite list is locally stored, but not movie details.
+
+@Table(name = "Favorites")
+public class DB_Favorite_Movies extends Model {
+
+    @Column(name = "tmdb_ID")
+    public long tmdb_ID;
+
+}
