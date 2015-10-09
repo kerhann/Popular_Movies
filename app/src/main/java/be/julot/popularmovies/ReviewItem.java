a67b0606ea50ce525bfd4b2b3c898a38b6221ddf
kerhann <jferet@gmail.com>
2015-10-09 17:54:04 +0200

    Attacking stage 2 - favorites seem to work

---
+++ app/src/main/java/be/julot/popularmovies/ReviewItem.java
@@ -0,0 +1,16 @@
+package be.julot.popularmovies;
+
+
+public class ReviewItem {
+
+    public String reviewer;
+    public String review;
+
+
+    public ReviewItem(String reviewer, String review) {
+        this.reviewer = reviewer;
+        this.review = review;
+
+    }
+
+}
