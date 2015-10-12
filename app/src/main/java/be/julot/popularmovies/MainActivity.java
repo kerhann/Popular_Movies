d2a38b7cdb07ac1702288aed175ce106420f35a1
kerhann <ULtc=gb6>
2015-10-12 23:34:21 +0200

    Ready for submission (hopefully!)

--- app/src/main/java/be/julot/popularmovies/MainActivity.java
+++ app/src/main/java/be/julot/popularmovies/MainActivity.java
@@ -15,11 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PosterGridFragment())
-                    .commit();
-        }
     }
 
     @Override
